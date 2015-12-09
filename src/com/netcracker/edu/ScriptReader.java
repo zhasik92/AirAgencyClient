63c4a343690c613a241c6d5e2d6cb16893172184
zhasik92 <zhasik140192@mail.ru>
2015-12-09 03:22:20 +0300

    readme added

--- src/com/netcracker/edu/ScriptReader.java
+++ src/com/netcracker/edu/ScriptReader.java
@@ -2,6 +2,7 @@ package com.netcracker.edu;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  * Testing server by reading commands from file, if expected value!=currentvalue, test will fail with exception
@@ -25,6 +26,7 @@ public class ScriptReader {
 
             String fuser, fserver;
             fuser = "sign_in " + args[2] + " " + args[3] + " ;0";
+            int requestCounter=0;
             while (true) {
                 if (fuser == null) {
                     br.close();
@@ -32,15 +34,26 @@ public class ScriptReader {
                     fuser = br.readLine();
                 }
                 String[] command = fuser.split(";");
+                Date time1=new Date();
+                requestCounter++;
+                if(requestCounter%500==0){
+                    System.out.println("number of requests= "+requestCounter);
+                }
+                System.out.println("sended request time:"+time1);
                 out.println(command[0]);
                 fserver = in.readLine();
+                Date time2=new Date();
+                System.out.println("get request time:"+time2);
                 int executionCode = Integer.parseInt(fserver);
-                System.out.println("currentValue = " + executionCode + ", expected value=" + command[1]);
+                //System.out.println("currentValue = " + executionCode + ", expected value=" + command[1]+" "+(time2.getTime()-time1.getTime()));
+                if((time2.getTime()-time1.getTime())>3000){
+                    System.out.println(time2.getTime()-time1.getTime());
+                }
                 if (Integer.parseInt(command[command.length - 1]) != -1) {
                     if (executionCode != Integer.parseInt(command[command.length - 1])) {
                         throw new Exception("Test failed: " + fuser + ", currentValue = " + executionCode + ", expected value=" + command[1]);
                     }else {
-                        System.out.println(executionCode + " unchecked command");
+                       // System.out.println(executionCode + " unchecked command");
                     }
                 }
                 fuser = br.readLine();
