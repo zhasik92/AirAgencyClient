cf46ea1689f126d39f8a65f31ab7840ce105d283
zhasik92 <zhasik140192@mail.ru>
2016-01-17 21:27:46 +0600

    readme added

--- src/com/netcracker/edu/ScriptReader.java
+++ src/com/netcracker/edu/ScriptReader.java
@@ -26,7 +26,7 @@ public class ScriptReader {
 
             String fuser, fserver;
             fuser = "sign_in " + args[2] + " " + args[3] + " ;0";
-            int requestCounter=0;
+            int requestCounter = 0;
             while (true) {
                 if (fuser == null) {
                     br.close();
@@ -34,26 +34,27 @@ public class ScriptReader {
                     fuser = br.readLine();
                 }
                 String[] command = fuser.split(";");
-                Date time1=new Date();
+                Date time1 = new Date();
                 requestCounter++;
-                if(requestCounter%500==0){
-                    System.out.println("number of requests= "+requestCounter);
+                if (requestCounter % 500 == 0) {
+                    System.out.println("number of requests= " + requestCounter);
                 }
-                System.out.println("sended request time:"+time1);
+                //System.out.println("sended request time:"+time1);
                 out.println(command[0]);
+                //System.out.println(command[0]);
                 fserver = in.readLine();
-                Date time2=new Date();
-                System.out.println("get request time:"+time2);
+                Date time2 = new Date();
+                //System.out.println("get request time:"+time2);
                 int executionCode = Integer.parseInt(fserver);
                 //System.out.println("currentValue = " + executionCode + ", expected value=" + command[1]+" "+(time2.getTime()-time1.getTime()));
-                if((time2.getTime()-time1.getTime())>3000){
-                    System.out.println(time2.getTime()-time1.getTime());
+                if ((time2.getTime() - time1.getTime()) > 3000) {
+                    System.out.println(time2.getTime() - time1.getTime());
                 }
                 if (Integer.parseInt(command[command.length - 1]) != -1) {
                     if (executionCode != Integer.parseInt(command[command.length - 1])) {
                         throw new Exception("Test failed: " + fuser + ", currentValue = " + executionCode + ", expected value=" + command[1]);
-                    }else {
-                       // System.out.println(executionCode + " unchecked command");
+                    } else {
+                        // System.out.println(executionCode + " unchecked command");
                     }
                 }
                 fuser = br.readLine();
